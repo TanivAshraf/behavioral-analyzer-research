7e71768efbda19c0a2841163bef9eff2b3f92bad
Taniv Ashraf <tanivashraf@gmail.com>
2025-07-30 17:35:19 +0600

    Update get-famous.js

--- api/get-famous.js
+++ api/get-famous.js
@@ -24,9 +24,8 @@ async function getGeminiAnalysis(text) {
 export default async function handler(req, res) {
     const { url, name } = req.query;
     // We are now using the ScrapingBee key
-    //const SCRAPINGBEE_API_KEY = process.env.SCRAPINGBEE_API_KEY;
-    // TEMPORARY DIAGNOSTIC TEST: HARDCODING THE KEY
-    const SCRAPINGBEE_API_KEY = "XV92IJPZFL4VPR79QLD7UUULT9V13UKOF9HQWCBVHD8QC1AB62LQD62R4KR735BC64S2ZAXJNOY70TLV";
+    const SCRAPINGBEE_API_KEY = process.env.SCRAPINGBEE_API_KEY;
+    
     try {
         if (!url || !name) {
             return res.status(400).json({ error: 'URL and name parameters are required.' });
